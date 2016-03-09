c49ccece7c53508920f8711a5289199936052793
ahmed reza <ahmedreza80@gmail.com>
2016-03-09 13:25:09 +0530

    added missing picf

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -3,3 +3,4 @@ class Gallery < ActiveRecord::Base
 	has_attached_file :image, styles: { medium: "100%x100%" }, :default_url => "missing.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
+
